c10e0171674c561d8870ad518b5014b10e69aeee
Pepitoelgringo <kxzdani@gmail.com>
2025-06-01 21:23:08 +0200

    Añadidos comentarios y mejoras minimas en codigo.

--- app/src/main/java/com/example/appemparejados/MainActivity.kt
+++ app/src/main/java/com/example/appemparejados/MainActivity.kt
@@ -1,9 +1,6 @@
 package com.example.appemparejados
 
-import android.animation.ObjectAnimator
-import android.app.AlertDialog
 import android.content.Context
-import android.content.DialogInterface
 import android.content.Intent
 import android.graphics.Color
 import android.graphics.Typeface
@@ -24,6 +21,7 @@ import androidx.appcompat.app.AppCompatActivity
 
 
 class MainActivity : AppCompatActivity() {
+    // DECLARACION DE COMPONENTES
     //<editor-fold desc="IMAGENES GUI">
     lateinit var iv_11:ImageView
     lateinit var iv_12:ImageView
@@ -62,6 +60,8 @@ class MainActivity : AppCompatActivity() {
     lateinit var imagen2:ImageView
 
     //</editor-fold>
+
+    // DECLARACION DE VARIABLES
     //<editor-fold desc="VARIABLES GUI">
     var imagenesArray = arrayOf(11,12,13,14,15,16,17,18,21,22,23,24,25,26,27,28)
     var homero = 0
@@ -117,23 +117,13 @@ class MainActivity : AppCompatActivity() {
         ib_sonido.setColorFilter(Color.BLACK)
 
         sonido("background",true)
-        iv_11.tag = "0"
-        iv_12.tag = "1"
-        iv_13.tag = "2"
-        iv_14.tag = "3"
-        iv_21.tag = "4"
-        iv_22.tag = "5"
-        iv_23.tag = "6"
-        iv_24.tag = "7"
-        iv_31.tag = "8"
-        iv_32.tag = "9"
-        iv_33.tag = "10"
-        iv_34.tag = "11"
-        iv_41.tag = "12"
-        iv_42.tag = "13"
-        iv_43.tag = "14"
-        iv_44.tag = "15"
 
+
+        imageViews.forEachIndexed { index, imageView -> // FOR EACH PARA ASIGNAR LAS TAGS, OPTIMIZADO CON forEachIndexed
+            imageView.tag = index.toString()
+        }
+
+        // ASIGNAMOS LOS RECURSOS A UNA VARIABLE
         homero = R.drawable.homero
         bart = R.drawable.bart
         comida = R.drawable.comida
@@ -143,8 +133,11 @@ class MainActivity : AppCompatActivity() {
         apu = R.drawable.apu
         barney = R.drawable.barney
 
+        // MEZCLAMOS EL ARRAY DE IMAGENES
         imagenesArray.shuffle()
 
+
+        // PREPARAMOS LOS TEXTOS Y LOS COLORES DEL TURNO
         tv_j1 = findViewById(R.id.tv_j1)
         tv_j2 = findViewById(R.id.tv_j2)
         tv_j2.setTextColor(Color.GRAY)
@@ -153,7 +146,9 @@ class MainActivity : AppCompatActivity() {
 
     }
 
+    // FUNCION PARA REPRODUCIR SONIDOS GUARDADOS EN LA CARPETA res/raw
     private fun sonido(sonidoName: String, loop: Boolean = false) {
+        // BUSCAMOS EL SONIDO QUE QUEREMOS REPRODUCIR CON EL NOMBRE Y GUARDAMOS SU ID
         val resID = resources.getIdentifier(sonidoName, "raw", packageName)
 
         if (sonidoName == "background") {
@@ -165,6 +160,7 @@ class MainActivity : AppCompatActivity() {
             }
         } else {
             mp = MediaPlayer.create(this,resID)
+            // LISTENER PARA QUE CUANDO TERMINE EL SONIDO, SE PARE Y LIBERE RECURSOS
             mp.setOnCompletionListener(MediaPlayer.OnCompletionListener { mediaPlayer ->
                 mediaPlayer.stop()
                 mediaPlayer.release()
@@ -174,6 +170,8 @@ class MainActivity : AppCompatActivity() {
             }
         }
     }
+
+    // METODO PARA ACTIVAR/DESACTIVAR LA MUSICA DE FONDO
     fun musicaFondo(v:View) {
         if (escuchar) {
             mpFondo.pause()
@@ -187,16 +185,176 @@ class MainActivity : AppCompatActivity() {
         escuchar = !escuchar
     }
 
+    // METODO PARA CUANDO HACES LICK EN UNA IMAGEN, REPRODUCE UN SONIDO Y VERIFICA LA IMAGEN
     fun seleccionar(imagen:View) {
         sonido("touch")
         verificar(imagen)
     }
 
+    // METODO QUE VERIFICA SI LA IMAGEN ES LA PRIMERA QUE ELIGES, O LA SEGUNDA
+    private fun verificar(imagen: View) {
+        // GUARDAMOS LA IMAGEN Y LA ETIQUETA EN VARIABLES
+        val iv = imagen as ImageView
+        val tag = imagen.tag.toString().toInt()
+
+        // MOSTAMOS LA CARTA CON ANIMACION
+        mostrarCartaConAnimacion(iv, tag)
+
+        //SI ES LA PRIMERA IMAGEN, LA GUARDAMOS EN imagen1 Y CAMBIAMOS LA VARIABLE numeroImagen A 2, PARA QUE LA SIGUIENTE NO ENTRE EN ESTE IF
+        if (numeroImagen == 1) {
+            imagen1 = iv
+            numeroImagen = 2
+            // DESACTIVAMOS EL LISTENER PARA QUE NO VUELVA A DAR CLICK EN ESTA IMAGEN
+            iv.isEnabled = false
+
+        // SI LA IMAGEN ES LA SEGUNDA, LA GUARDAMOS EN imagen2 Y CAMBIAMOS LA VARIABLE numeroImagen A 1, PARA QUE LA SIGUIENTE NO ENTRE EN ESTE IF
+        } else if (numeroImagen == 2) {
+            imagen2 = iv
+            numeroImagen = 1
+            // DESACTIVAMOS EL LISTENER PARA QUE NO VUELVA A DAR CLICK EN ESTA IMAGEN
+            iv.isEnabled = false
+
+            // DESHABILITAMOS LAS IMAGENES
+            deshabilitarImagenes()
+
+            // COGE EL HILO PRINCIPAL DONDE SE ESTA EJECUTANDO LA APLICACION, Y PASADOS EL TIEMPO ESTABLECIDO
+            // (1000 MILISEGUNDOS EN ESTE CASO) Y EJECUTA EL CODIGO, EN ESTE CASO COMPROBAR SI SON IMAGENES IGUALES
+            android.os.Handler(Looper.getMainLooper()).postDelayed({
+                // METODO QUE COMPRUEBA SI LAS IMAGENES SON IGUALES
+                sonImagenesIguales()
+            }, 1000)
+        }
+    }
+
+    // METODO QUE COMPRUEBA SI LAS IMAGENES SON IGUALES
+    private fun sonImagenesIguales() {
+        // COMPRUEBA SI LAS IMAGENES COINCIDEN
+        if (imagen1.drawable.constantState == imagen2.drawable.constantState) {
+            // SI COINCIDEN, MOSTRAMOS UNA ANIMACION Y REPRODUCIMOS UN SONIDO
+            animarAcierto(imagen1)
+            animarAcierto(imagen2)
+            sonido("success")
+
+            // ACTUALIZAMOS LA VARIABLE DEL BONUS DE PUNTOS POR SI ACIERTA VARIOS VECES SEGUIDAS
+            val puntosAcumulados = 1 + bonus
+
+            //SUMAMOS LOS PUNTOS AL JUGADOR CORRESPONDIENTE Y MOSTRAMOS UNA ANIMACION
+            if (turno == 1) {
+                puntosj1 += puntosAcumulados
+                tv_j1.text = "J1: $puntosj1"
+                bonus = (bonus + 1).coerceAtMost(3)
+                animarCombo(this, findViewById(R.id.fl_contenedor_combo), "¡$bonus x COMBO!")
+            } else if (turno == 2) {
+                puntosj2 += puntosAcumulados
+                tv_j2.text = "J2: $puntosj2"
+                bonus = (bonus + 1).coerceAtMost(3)
+                animarCombo(this, findViewById(R.id.fl_contenedor_combo), "¡$bonus x COMBO!")
+            }
+            // COMO HA ACERTADO, DESACTIVAMOS LAS IMAGENES Y LAS ETIQUETAS
+            imagen1.isEnabled = false
+            imagen2.isEnabled = false
+            imagen1.tag = ""
+            imagen2.tag = ""
+
+        } else { // SI LAS IMAGENES NO COINCIDEN
+            // HACEMOS VIBRAR EL MOVIL
+            vibracionMovil(this)
+            // RESETEAMOS EL BONUS
+            bonus = 0
+            // REPRODUCIMOS UN SONIDO DE ERROR
+            sonido("no")
+            // MOSTRAMOS ANIMACION DE ERROR
+            animarError(imagen1)
+            animarError(imagen2)
+            // VOLVEMOS A OCULTAR LAS IMAGENES
+            imagen1.setImageResource(R.drawable.oculta)
+            imagen2.setImageResource(R.drawable.oculta)
+
+            // IF PARA ANIMACION DE QUIEN PASA A SER EL TURNO
+            if (turno==1) {
+                animarAnuncio(tv_anuncio,
+                    getString(R.string.turno_del_jugador_2),getResources().getColor(R.color.azulJugador1))
+                turno = 2
+                tv_j1.setTextColor(Color.GRAY)
+                tv_j2.setTextColor(getResources().getColor(R.color.azulJugador1))
+            } else if (turno==2) {
+                animarAnuncio(tv_anuncio,
+                    getString(R.string.turno_del_jugador_1),getResources().getColor(R.color.rojoJugador2))
+                turno = 1
+                tv_j2.setTextColor(Color.GRAY)
+                tv_j1.setTextColor(getResources().getColor(R.color.rojoJugador2))
+            }
+        }
+
+        // SI LA ETIQUETA DE LA IMAGEN NO ESTA VACIA, ACTIVAMOS LA IMAGEN
+        iv_11.isEnabled = iv_11.tag.toString().isNotEmpty()
+        iv_12.isEnabled = iv_12.tag.toString().isNotEmpty()
+        iv_13.isEnabled = iv_13.tag.toString().isNotEmpty()
+        iv_14.isEnabled = iv_14.tag.toString().isNotEmpty()
+        iv_21.isEnabled = iv_21.tag.toString().isNotEmpty()
+        iv_22.isEnabled = iv_22.tag.toString().isNotEmpty()
+        iv_23.isEnabled = iv_23.tag.toString().isNotEmpty()
+        iv_24.isEnabled = iv_24.tag.toString().isNotEmpty()
+        iv_31.isEnabled = iv_31.tag.toString().isNotEmpty()
+        iv_32.isEnabled = iv_32.tag.toString().isNotEmpty()
+        iv_33.isEnabled = iv_33.tag.toString().isNotEmpty()
+        iv_34.isEnabled = iv_34.tag.toString().isNotEmpty()
+        iv_41.isEnabled = iv_41.tag.toString().isNotEmpty()
+        iv_42.isEnabled = iv_42.tag.toString().isNotEmpty()
+        iv_43.isEnabled = iv_43.tag.toString().isNotEmpty()
+        iv_44.isEnabled = iv_44.tag.toString().isNotEmpty()
+
+        // LLAMAMOS AL METODO PARA VERIFICAR SI SE HAN DADO LA VUELTA A TODAS LAS IMAGENES
+        verificarFinjuego()
+    }
+
+    // VERIFICA SI ES EL FINAL DEL JUEGO (TODAS LAS CARTAS HAN SIDO DADAS LA VUELTA)
+    private fun verificarFinjuego() {
+        // SI TODAS LAS ETIQUETAS ESTAN VACIAS
+        if (imageViews.all { it.tag.toString().isEmpty() }) {
+            // PARAMOS LA MUSICA
+            mp.stop()
+            mp.release()
+            // MOSTRAMOS ANIMACION DE FIN DE JUEGO
+            animarFinalJuego(imageViews)
+            // REPRODUCIMOS EL SONIDO DE LA VICTORIA
+            sonido("win")
+
+            // CREAMOS UNA INSTANCIA DEL DIALOG PARA MOSTRAR LA PUNTUACION
+            val dialog = dialogFinJuego()
+
+
+            // ENVIAMOS AL FRAGMENT LA PUNTUACION A TRAVES DEL BUNDLE
+            dialog.arguments = Bundle().apply {
+                putInt("puntosJugador1",puntosj1)
+                putInt("puntosJugador2",puntosj2)
+            }
+
+            // DEFINIMOS ACCIONES PARA LOS BOTONES DEL FRAGMENT
+            dialog.setAcciones(
+                // SI LE DA A JUGAR DE NUEVO, ABRIMOS UNA NUEVA ACTIVITY Y FINIALIZAMOS LA DE ANTES
+                jugarDeNuevo = {
+                    val intent = Intent(this, MainActivity::class.java)
+                    startActivity(intent)
+                    finish()
+                },
+                // SI LE DA A FINALIZAR CERRAMOS LA APLICACION
+                salir = {
+                    finish()
+                }
+            )
+
+            dialog.show(supportFragmentManager, getString(R.string.dialogofinaljuego))
+        }
+    }
+
+    // METODO PARA MOSTRAR LA CARTA AL JUGADOR CON UNA ANIMACION
     private fun mostrarCartaConAnimacion(view: ImageView, tag: Int) {
         view.animate()
             .rotationY(90f)
             .setDuration(150)
             .withEndAction {
+                // CON LA ETIQUETA PROPORCIONADA EN LOS PARAMETROS, PODEMOS SETEAR LA IMAGEN CORRECTA EN EL IMAGEVIEW QUE HA CLICKEADO EL USUARIO
                 val nuevaImagen = when (imagenesArray[tag]) {
                     11 -> R.drawable.homero
                     12 -> R.drawable.bart
@@ -216,8 +374,8 @@ class MainActivity : AppCompatActivity() {
                     28 -> R.drawable.barney
                     else -> R.drawable.oculta
                 }
+                // SETEAMOS LA IMAGEN EN EL ImageView
                 view.setImageResource(nuevaImagen)
-
 
                 view.rotationY = -90f
                 view.animate()
@@ -229,154 +387,7 @@ class MainActivity : AppCompatActivity() {
     }
 
 
-    private fun verificar(imagen: View) {
-        var iv = imagen as ImageView
-        var tag = imagen.tag.toString().toInt()
-
-        mostrarCartaConAnimacion(iv, tag)
-
-        if (numeroImagen == 1) {
-            imagen1 = iv
-            numeroImagen = 2
-            iv.isEnabled = false
-        } else if (numeroImagen == 2) {
-            imagen2 = iv
-            numeroImagen = 1
-            iv.isEnabled = false
-
-            deshabilitarImagenes()
-            android.os.Handler(Looper.getMainLooper()).postDelayed({
-                sonImagenesIguales()
-            }, 1000)
-        }
-    }
-
-
-    private fun sonImagenesIguales() {
-        if (imagen1.drawable.constantState == imagen2.drawable.constantState) {
-            animarAcierto(imagen1)
-            animarAcierto(imagen2)
-            sonido("success")
-
-            val puntosAcumulados = 1 + bonus
-
-            if (turno == 1) {
-                puntosj1 += puntosAcumulados
-                tv_j1.text = "J1: $puntosj1"
-                bonus = (bonus + 1).coerceAtMost(3)
-                animarCombo(this, findViewById(R.id.fl_contenedor_combo), "¡$bonus x COMBO!")
-            } else if (turno == 2) {
-                puntosj2 += puntosAcumulados
-                tv_j2.text = "J2: $puntosj2"
-                bonus = (bonus + 1).coerceAtMost(3)
-                animarCombo(this, findViewById(R.id.fl_contenedor_combo), "¡$bonus x COMBO!")
-            }
-            imagen1.isEnabled = false
-            imagen2.isEnabled = false
-            imagen1.tag = ""
-            imagen2.tag = ""
-        } else {
-            vibracionMovil(this)
-            bonus = 0
-            sonido("no")
-            animarError(imagen1)
-            animarError(imagen2)
-            imagen1.setImageResource(R.drawable.oculta)
-            imagen2.setImageResource(R.drawable.oculta)
-            if (turno==1) {
-                animarAnuncio(tv_anuncio,
-                    getString(R.string.turno_del_jugador_2),getResources().getColor(R.color.azulJugador1))
-                turno = 2
-                tv_j1.setTextColor(Color.GRAY)
-                tv_j2.setTextColor(getResources().getColor(R.color.azulJugador1))
-            } else if (turno==2) {
-                animarAnuncio(tv_anuncio,
-                    getString(R.string.turno_del_jugador_1),getResources().getColor(R.color.rojoJugador2))
-                turno = 1
-                tv_j2.setTextColor(Color.GRAY)
-                tv_j1.setTextColor(getResources().getColor(R.color.rojoJugador2))
-            }
-        }
-        iv_11.isEnabled = iv_11.tag.toString().isNotEmpty()
-        iv_12.isEnabled = iv_12.tag.toString().isNotEmpty()
-        iv_13.isEnabled = iv_13.tag.toString().isNotEmpty()
-        iv_14.isEnabled = iv_14.tag.toString().isNotEmpty()
-        iv_21.isEnabled = iv_21.tag.toString().isNotEmpty()
-        iv_22.isEnabled = iv_22.tag.toString().isNotEmpty()
-        iv_23.isEnabled = iv_23.tag.toString().isNotEmpty()
-        iv_24.isEnabled = iv_24.tag.toString().isNotEmpty()
-        iv_31.isEnabled = iv_31.tag.toString().isNotEmpty()
-        iv_32.isEnabled = iv_32.tag.toString().isNotEmpty()
-        iv_33.isEnabled = iv_33.tag.toString().isNotEmpty()
-        iv_34.isEnabled = iv_34.tag.toString().isNotEmpty()
-        iv_41.isEnabled = iv_41.tag.toString().isNotEmpty()
-        iv_42.isEnabled = iv_42.tag.toString().isNotEmpty()
-        iv_43.isEnabled = iv_43.tag.toString().isNotEmpty()
-        iv_44.isEnabled = iv_44.tag.toString().isNotEmpty()
-
-        verificarFinjuego()
-    }
-
-    private fun verificarFinjuego() {
-        if (imageViews.all { it.tag.toString().isEmpty() }) {
-            mp.stop()
-            mp.release()
-            animarFinalJuego(imageViews)
-            sonido("win")
-
-            // CREAMOS UNA INSTANCIA DEL DIALOG
-            val dialog = dialogFinJuego()
-
-
-            dialog.arguments = Bundle().apply {
-                putInt("puntosJugador1",puntosj1)
-                putInt("puntosJugador2",puntosj2)
-            }
-
-            dialog.setAcciones(
-                jugarDeNuevo = {
-                    val intent = Intent(this, MainActivity::class.java)
-                    startActivity(intent)
-                    finish()
-                },
-                salir = {
-                    finish()
-                }
-            )
-
-            dialog.show(supportFragmentManager, getString(R.string.dialogofinaljuego))
-        }
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        if (mpFondo.isPlaying) {
-            mpFondo.stop()
-            mpFondo.release()
-        }
-    }
-
-    private fun deshabilitarImagenes() {
-        iv_11.isEnabled = false
-        iv_12.isEnabled = false
-        iv_13.isEnabled = false
-        iv_14.isEnabled = false
-        iv_21.isEnabled = false
-        iv_22.isEnabled = false
-        iv_23.isEnabled = false
-        iv_24.isEnabled = false
-        iv_31.isEnabled = false
-        iv_32.isEnabled = false
-        iv_33.isEnabled = false
-        iv_34.isEnabled = false
-        iv_41.isEnabled = false
-        iv_42.isEnabled = false
-        iv_43.isEnabled = false
-        iv_44.isEnabled = false
-
-    }
-}
-
+// METODO PARA ANIMAR ERROR
 fun animarError(carta: ImageView) {
     carta.animate()
         .translationXBy(20f) // MOVEMOS LA CARTA A LA DERECHA
@@ -393,6 +404,7 @@ fun animarError(carta: ImageView) {
         }
 }
 
+// METODO PARA ANIMAR ACIERTO
 fun animarAcierto(carta: ImageView) {
     carta.animate()
         //AUMENTAMOS EL TAMAÑO VERTICAL Y HORIZONTAL
@@ -409,6 +421,7 @@ fun animarAcierto(carta: ImageView) {
         }
 }
 
+// METODO PARA ANIMAR EL FINAL DEL JUEGO
 fun animarFinalJuego(imageViews: List<ImageView>) {
     imageViews.forEach { imageView ->
         imageView.animate()
@@ -427,6 +440,7 @@ fun animarFinalJuego(imageViews: List<ImageView>) {
     }
 }
 
+// METODO PARA ANIMAR EL COMBO DE PUNTUACION
 fun animarCombo(context: Context, parent: FrameLayout, combo: String) {
     // CREAR UN NUEVO TextView Y CONFIGURAR SUS ATRIBUTOS
     val textView = TextView(context).apply {
@@ -470,6 +484,7 @@ fun animarCombo(context: Context, parent: FrameLayout, combo: String) {
         .start() // INICIAR LA PRIMERA PARTE DE LA ANIMACIÓN
 }
 
+// METODO PARA QUE VIBRE EL MOVIL
 fun vibracionMovil(context: Context) {
     // OBTENEMOS EL SERVICIO DE VIBRACIÓN DEL SISTEMA
     val vibrator = context.getSystemService(Context.VIBRATOR_SERVICE) as Vibrator
@@ -490,12 +505,13 @@ fun vibracionMovil(context: Context) {
     }
 }
 
+// METODO PARA ANIMAR EL TextView DE CAMBIO DE TURNO
 fun animarAnuncio(textView: TextView, anuncio: String, colorJugador:Int) {
     textView.setTextColor(colorJugador)
 
     // ESCALA DEL TEXTVIEW (EFECTO DE CRECER Y LUEGO VOLVER A SU TAMAÑO ORIGINAL)
-    val scaleX = android.animation.ObjectAnimator.ofFloat(textView, "scaleX", 1f, 1.5f, 1f) // Animación de escala horizontal
-    val scaleY = android.animation.ObjectAnimator.ofFloat(textView, "scaleY", 1f, 1.5f, 1f) // Animación de escala vertical
+    val scaleX = android.animation.ObjectAnimator.ofFloat(textView, "scaleX", 1f, 1.5f, 1f)
+    val scaleY = android.animation.ObjectAnimator.ofFloat(textView, "scaleY", 1f, 1.5f, 1f)
 
     // DURACIÓN DE LA ANIMACIÓN (500 MILISEGUNDOS)
     scaleX.duration = 500
@@ -507,4 +523,20 @@ fun animarAnuncio(textView: TextView, anuncio: String, colorJugador:Int) {
 
     // CAMBIAMOS EL TEXTO A "ES EL TURNO DE [NOMBRE]" O LO QUE QUIERAS
     textView.text = anuncio
+}
+
+
+    // SI SE DESTRUYE LA ACTIVIDAD, PARAMOS EL SONIDO, Y LIBERAMOS RECURSOS
+    override fun onDestroy() {
+        super.onDestroy()
+        if (mpFondo.isPlaying) {
+            mpFondo.stop()
+            mpFondo.release()
+        }
+    }
+
+    // METODO PARA DESHABILITAR LAS IMAGENES
+    private fun deshabilitarImagenes() { // METODO PARA DESHABILITAR IMAGENES, OPTIMIZADO CON forEach
+        imageViews.forEach { it.isEnabled = false }
+    }
 }
